238792e0249cc68e20e1cea9814050d74110d66d
kaio <kaio.silva@calindra.com.br>
2023-05-07 13:53:30 -0300

    adicionando início da implementação do firebase - falta apenas as cred do firebase

---
+++ src/service/firebase/signIn.js
@@ -0,0 +1,22 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "./config";
+
+export const signIn = (email, senha) => {
+  signInWithEmailAndPassword(auth, email, senha)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log(`Conta logada com sucesso no Firebase! Usuário: ${JSON.stringify(user)}`);
+    })
+    .catch((error) => {
+      if (
+        error.message.includes("auth/wrong-password") ||
+        error.message.includes("firebase.login.error")
+      ) {
+        Alert.alert("Email ou senha inválido. Tente novamente.");
+      } else {
+        Alert.alert("Erro inesperado, tente novamente mais tarde.");
+      }
+
+      throw new Error("firebase.login.error: ", error);
+    });
+};
---
+++ src/service/firebase/signUp.js
@@ -0,0 +1,19 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "./config";
+
+const signUp = (email, senha) => {
+  createUserWithEmailAndPassword(auth, email, senha)
+    .then((userCredential) => {
+      const user = userCredential.user
+      console.log(`Conta criada com sucesso no Firebase! Usuário: ${JSON.stringify(user)}`);
+    })
+    .catch((error) => {
+      if (error.message.includes("email-already-in-use")) {
+        Alert.alert("E-mail já cadastrado.");
+      } else {
+        Alert.alert("Erro inesperado, tente novamente mais tarde.");
+      }
+
+      throw new Error("firebase.create.account.error: ", error);
+    });
+};
