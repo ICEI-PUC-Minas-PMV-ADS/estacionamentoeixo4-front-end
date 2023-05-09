4c81490e823a133e7caa88aa55a102c18c7655f3
kaio <kaio.silva@calindra.com.br>
2023-05-08 21:13:45 -0300

    adicionando credenciais

--- src/service/firebase/config.js
+++ src/service/firebase/config.js
@@ -1,15 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-//TODO: Pegar valores das credenciais abaixo.
-
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyCnt9CBCcPzeWBbBNHcdlicnPo7lDJf4Pc",
+  authDomain: "whypark-ab6d6.firebaseapp.com",
+  projectId: "whypark-ab6d6",
+  storageBucket: "whypark-ab6d6.appspot.com",
+  messagingSenderId: "887804319158",
+  appId: "1:887804319158:web:d901ee288addfd74819d17"
 };
 
 const app = initializeApp(firebaseConfig);
